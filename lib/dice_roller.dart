922458ed09fe3e1c05d63457df1ce83d33403e14
Chris Nzoka-Okoye <chrisnzokaokoye@gmail.com>
2025-02-08 07:17:29 +0100

    updated the setState function

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class DiceRoller extends StatefulWidget {
@@ -13,9 +15,12 @@ class _DiceRollerState extends State<DiceRoller> {
   var activeDiceImage = 'assets/images/dice-3.png';
   // The rollDice function serves as the Textbutton onPressed function
   void rollDice() {
+    // Random().nextInt(6) + 1 is assigned to a variable and added to setState using $
+    var diceRoll = Random().nextInt(6) + 1; // gives number b/w 1 & 6
+    //Random().nextInt(6) gives number b/w 0 - 5
     setState(
       () {
-        activeDiceImage = 'assets/images/dice-2.png';
+        activeDiceImage = 'assets/images/dice-$diceRoll.png';
       },
     );
     //the content of this function is how to NOT build interactive widget
