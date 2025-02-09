e8d7232ee26f0520070d581f514f4cf441d2b8d1
Chris Nzoka-Okoye <chrisnzokaokoye@gmail.com>
2025-02-09 13:45:15 +0100

    updated the setState function

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -12,18 +12,17 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-3.png';
+  var currentDiceRoll = 2;
   // The rollDice function serves as the Textbutton onPressed function
   void rollDice() {
-    // Random().nextInt(6) + 1 is assigned to a variable and added to setState using $
-    var diceRoll = Random().nextInt(6) + 1; // gives number b/w 1 & 6
-    //Random().nextInt(6) gives number b/w 0 - 5
     setState(
       () {
-        activeDiceImage = 'assets/images/dice-$diceRoll.png';
+        currentDiceRoll= Random().nextInt(6) + 1;
+        // gives number b/w 1 & 6
+        //Random().nextInt(6) gives number b/w 0 - 5
+        // Random().nextInt(6) + 1 is assigned to a variable and added to setState using $
       },
-    );
-    //the content of this function is how to NOT build interactive widget
+  );
   }
 
   @override
@@ -32,7 +31,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min, //centers column content vertically
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/image/dice-$currentDiceRoll.png',
           width: 200,
         ),
         //SizedBox(height: 20,) alternative to adding padding within the text widget
